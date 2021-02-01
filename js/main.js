08a2ff0710e1186b24efbeb955c185e05bd1f7f4
SheMortal <mortalshe@gmail.com>
2021-02-01 03:27:44 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($) {
+
+  'use strict';
+  
+        $(window).on("scroll", function() {
+            if($(window).scrollTop() > 100) {
+                $(".header").addClass("active");
+            } else {
+                //remove the background property so it comes transparent again (defined in css)
+               $(".header").removeClass("active");
+            }
+        });
+
+
+	/************** Mixitup (Filter Projects) *********************/
+    	$('.projects-holder').mixitup({
+            effects: ['fade','grayscale'],
+            easing: 'snap',
+            transitionSpeed: 400
+        });
+
+});
