d711c1f89c6ea408e1e9245c6800375bb4fec911
SheMortal <mortalshe@gmail.com>
2021-02-09 06:57:30 +0200

    updates

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
 jQuery(document).ready(function($) {
 
-  'use strict';
-  
-        $(window).on("scroll", function() {
-            if($(window).scrollTop() > 100) {
-                $(".header").addClass("active");
-            } else {
-                //remove the background property so it comes transparent again (defined in css)
-               $(".header").removeClass("active");
-            }
-        });
+    'use strict';
+
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > 100) {
+            $(".header").addClass("active");
+        } else {
+            //remove the background property so it comes transparent again (defined in css)
+            $(".header").removeClass("active");
+        }
+    });
 
 
-	/************** Mixitup (Filter Projects) *********************/
-    	$('.projects-holder').mixitup({
-            effects: ['fade','grayscale'],
-            easing: 'snap',
-            transitionSpeed: 400
-        });
+    /************** Mixitup (Filter Projects) *********************/
+    $('.projects-holder').mixitup({
+        effects: ['fade', 'grayscale'],
+        easing: 'snap',
+        transitionSpeed: 400
+    });
 
 });
 
+function preloaderFadeOutInit() {
+    $('.preloader').fadeOut('slow');
+    $('body').attr('id', '');
+}
+// Window load function
+jQuery(window).on('load', function() {
+    (function($) {
+        preloaderFadeOutInit();
+    })(jQuery);
+});
